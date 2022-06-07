cb821626a6d44eb7f7a78c6157f9e12e799e575a
xzlyf <czr2001@outlook.com>
2022-06-07 17:54:53 +0800

    全局异常处理模块独立化

--- src/main/java/com/xz/ppjnet/PpjNetApplication.java
+++ src/main/java/com/xz/ppjnet/PpjNetApplication.java
@@ -2,12 +2,14 @@ package com.xz.ppjnet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//扫描子模块，不然子模块的注解不生效
+@ComponentScan({"com.xz.ppjnet", "com.xz.module.common"})
 public class PpjNetApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PpjNetApplication.class, args);
     }
-
 }
